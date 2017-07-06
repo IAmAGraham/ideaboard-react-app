295ce9f5536a1504888b07ea9c88f18cce30d7d6
github username <github email address>
2017-07-06 09:08:14 -0400

    cant seem to save content in postit this is a commit before I break everything

--- ideaboard-react/src/components/IdeaBoardDetail.js
+++ ideaboard-react/src/components/IdeaBoardDetail.js
@@ -3,8 +3,49 @@ import {Link} from 'react-router-dom'
 import IdeaBoardItem from './IdeaBoardItem'
 
 export default class IdeaBoardDetail extends Component {
-  constructor(props){
-    super(props)
+  constructor(){
+    super();
+    this.state = {
+      add: true,
+      stickies: []
+    }
+  }
+
+  update = (newText, id) => {
+    this.setState(previousState => {
+      let stickies = previousState.stickies.map( sticky =>(
+        sticky.id !== id ? sticky : {...sticky, sticky: newText }
+      ))
+      return {
+        stickies: stickies
+      }
+    })
+  }
+
+  add = () => {
+    this.setState(previousState=>(
+      {
+        stickies: [...previousState.stickies, { id: this.nextId(), sticky: 'New Sticky'}]
+      }
+    ))
+  }
+
+  nextId(){
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+  }
+
+  remove = (id) =>{
+    this.setState(previousState =>{
+      let stickies = previousState.stickies.filter(sticky => sticky.id !== id)
+      return {stickies: stickies}
+    })
+  }
+
+  eachSticky = (sticky) =>{
+    return(
+      <IdeaBoardItem key={sticky.id} id={sticky.id} onSave={this.update} onSubmit={this.submit} onRemove={this.remove}>{sticky.sticky}</IdeaBoardItem>
+    )
   }
 
   render(){
@@ -15,10 +56,10 @@ export default class IdeaBoardDetail extends Component {
     return (
       <div>
       <h2>{this.props.board.title}</h2>
-      <IdeaBoardItem onSubmit={this.createSticky}/>
+
+        {this.state.stickies.map(this.eachSticky)}
+        <button onClick={this.add}>+</button>
       </div>
-
     )
-
   }
 }
--- ideaboard-react/src/components/IdeaBoardItem.js
+++ ideaboard-react/src/components/IdeaBoardItem.js
@@ -8,32 +8,147 @@ export default class IdeaBoardItem extends Component{
 
     constructor(props){
       super(props);
+
+      this.handleTextChange = this.handleTextChange.bind(this);
+      this.handleSave = this.handleSave.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.renderForm = this.renderForm.bind(this);
+      this.renderStickyInput = this.renderStickyInput.bind(this);
+      this.renderDisplay = this.renderDisplay.bind(this);
+      this.renderStickyInput = this.renderStickyInput.bind(this);
+      this.save = this.save.bind(this);
       this.state = {
-        boards: []
+        boards: [],
+        editing: false,
+        stickies: [{id: 1, content:'', x:'', y:''}]
       }
     }
 
+    edit = () =>{
+      this.setState({editing: true})
+    }
+
+    componentWillMount(){
+      this.style = {
+        right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+        top: this.randomBetween(0, window.innerHeight - 150, 'px')
+      }
+    }
+
+    componentDidUpdate(){
+      if (this.state.editing) {
+        // debugger
+        this.refs.newText
+
+      }
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+      return this.props.value !== nextProps.value || this.state !== nextState.value
+    }
+
+    save(event){
+      event.preventDefault()
+      const content = `${this.state.stickies[0].content}`
+
+      this.props.onSave(content)
+      this.setState({
+        content: ''
+      })
+    }
+
+
+    handleSave(event){
+      event.preventDefault()
+      // debugger
+      if(this.state.content !== ""){
+        this.props.on
+      }
+    }
+
+    randomBetween(x,y,s){
+      return (x + Math.ceil(Math.random()*(y-x))) +s
+    }
+
+    remove = () =>{
+      this.props.onRemove(this.props.id)
+    }
+
+    handleSubmit(event){
+      event.preventDefault()
+      // debugger
+      const content = `${this.state.stickies[0].content}`
+      this.props.onSave(content)
+      this.setState({
+        content: ''
+      })
+    }
+
+    handleTextChange(id, content){
+      this.setState((previousState) => {
+        return {
+          stickies: previousState.stickies.map((stick) => {
+            if (stick.id !== id) {
+              return stick
+            } else {
+              return {id: id, content: content}
+            }
+          })
+        }
+      })
+    }
+
+
+    renderForm(){
+      return (
+      <div className="container bootstrap snippet">
+        <div className="row">
+          <ul className="notes">
+          <li>
+            <div className="rotate-1 lazur-bg">
+            {this.renderStickyInput()}
+            </div>
+          </li>
+          </ul>
+        </div>
+      </div>
+    )}
+
+    renderStickyInput(){
+      return this.state.stickies.map( stick => (
+        <form onSubmit={this.handleSubmit}>
+        <div>
+          <textarea key={stick.id} type='text' id={stick.id} placeholder="Add Sticky Stuff" defaultValue={stick.content} onChange={(event) => this.handleTextChange(stick.id, event.target.value)} />
+          <button onClick={this.save}>Save</button>
+        </div>
+        </form>
+      ))
+    }
+
+    renderDisplay(){
+      return(
+      <div className="container bootstrap snippet" style={this.style}>
+        <div className="row">
+          <ul className="notes">
+          <li>
+            <div className="rotate-1 lazur-bg">
+              <button onClick={this.remove}>X</button>
+              <button onClick={this.edit}>Edit</button>
+            </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )}
+
 
     render(){
-      debugger
       return(
-
-
-
+        <div>
         <Draggable>
-        <div className="container bootstrap snippet">
-        <div className="row">
-        <ul className="notes">
-        <li >
-        <div className="rotate-1 lazur-bg">
-        <textarea>You can type in here</textarea>
-
-        </div>
-        </li>
-        </ul>
-        </div>
-        </div>
+          {(this.state.editing) ? this.renderForm() : this.renderDisplay()}
         </Draggable>
+        </div>
       )
     }
 }
